b52564c9641975187c415f95dd98fa6b794d0e83
AlexandreHumbert <alexandre.humbert79@gmail.com>
2022-07-21 17:27:31 +0200

    feat car routes

--- app/router/index.js
+++ app/router/index.js
@@ -3,11 +3,11 @@ const router = Router();
 
 import { router as usersRouter} from './users.js';
 import { router as authRouter } from './auth.js';
-
-
-
+import { router as carRouter } from './car.js';
 
 router.use('/api/users', usersRouter);
 router.use('/api/auth', authRouter);
+router.use('/', carRouter);
+
 
 export { router };
