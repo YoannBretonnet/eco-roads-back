7b1fdd435618a456e134c6b107484e195976f0f4
GaetanSantucci <99250736+GaetanSantucci@users.noreply.github.com>
2022-07-23 22:47:08 +0200

    :ambulance: fix: uninstall and reinstall node module to try to fix heroku deploy

--- index.js
+++ index.js
@@ -1,49 +1,48 @@
 // ~ *** *** ENVIRONMENT CONFIG *** *** ~ //
 // ~ ********************************** ~ //
 
-import 'dotenv/config';
+import "dotenv/config";
 
 // ~ *** *** EXPRESS CONFIG *** *** ~ //
 // ~ ****************************** ~ //
 
-import express from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import { dirname, join } from 'path';
-import { fileURLToPath } from 'url';
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 
-import { router } from './app/router/index.js';
+import { router } from "./app/router/index.js";
 
 const app = express();
 
 // If you have your node.js behind a proxy and are using secure: true, you need to set 'trust proxy' in express
-// app.set('trust proxy', 1) // trust first proxy
+app.set("trust proxy", 1); // trust first proxy
 
 // ~ *** *** SESSION CONFIG *** *** ~ //
 // ~ ****************************** ~ //
 
-import session from 'express-session';
+import session from "express-session";
 
-app.use(session({
-    saveUninitialized: true,
-    resave: true,
-    secret: process.env.SESSION_SECRET,
-    cookie: { 
-        // secure : true,
-        httpOnly : true,
-        sameSite: 'lax', // or 'strict'
-        maxAge: 24 * 60 * 60 * 1000 //24 hours
-        //expires : new Date(Date.now() + 60 * 60 * 1000) //1 hour
-        }
-}));
-
-
+app.use(
+    session({
+        saveUninitialized: true,
+        resave: true,
+        secret: process.env.SESSION_SECRET,
+        cookie: {
+            // secure : true,
+            httpOnly: true,
+            sameSite: "lax", // or 'strict'
+            maxAge: 24 * 60 * 60 * 1000, //24 hours
+            //expires : new Date(Date.now() + 60 * 60 * 1000) //1 hour
+        },
+    }),
+);
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const PORT = process.env.PORT || 5000;
-const corsOptions = { credentials:true, origin: process.env.URL || '*'};
-
+const corsOptions = { credentials: true, origin: process.env.URL || "*" };
 
 app.use(cors(corsOptions));
 
@@ -54,15 +53,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/', express.static(join(__dirname, 'public')));
+app.use("/", express.static(join(__dirname, "public")));
 
-app.use(router)
+app.use(router);
 
 // ~ *** *** LAUNCHER CONFIG *** *** ~ //
 // ~ ******************************* ~ //
 
 app.use(router);
 
-app.listen(PORT, ()=>{
-    console.log(` \x1b[1;33m⚡⚡ http://localhost:${PORT} ⚡⚡ \x1b[0m`)
+app.listen(PORT, () => {
+    console.log(` \x1b[1;33m⚡⚡ http://localhost:${PORT} ⚡⚡ \x1b[0m`);
 });
