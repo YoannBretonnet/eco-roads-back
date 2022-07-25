e443c58d04855db26d7b1b0fc5936e857a0eaa5c
GaetanSantucci <99250736+GaetanSantucci@users.noreply.github.com>
2022-07-26 00:22:05 +0200

    :bug: fix bug accestoken

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -1,21 +1,18 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
-function authenticateToken(req,res,next){
+function authenticateToken(req, res, next) {
+    const accessToken = req.cookies.accessToken;
 
-    const accessToken = req.cookies.accessToken
-    
     // const authHeader = req.headers['authorization']; //Bearer TOKEN
     // const token = authHeader && authHeader.split(' ')[1];
 
-    
-    if (accessToken == null) return res.status(401).json({error: "Token nul"});
-    
-    jwt.verify(tokenACCESS_TOKEN,process.env.ACCESS_TOKEN_SECRET, (error,user) =>{
-    
-        if(error) return res.status(403).json({ error : error.message });
+    if (accessToken == null) return res.status(401).json({ error: "Token nul" });
+
+    jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (error, user) => {
+        if (error) return res.status(403).json({ error: error.message });
         req.user = user;
         next();
-    })
+    });
 }
 
 export { authenticateToken };
