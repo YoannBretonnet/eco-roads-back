41c6ccc19fc4b58b02316c44f4d9e46b38341128
GaetanSantucci <99250736+GaetanSantucci@users.noreply.github.com>
2022-07-26 09:20:39 +0200

    :boom: change cors, test setHeader

--- index.js
+++ index.js
@@ -16,13 +16,11 @@ const app = express();
 
 // ~ *** *** SWAGGER CONFIG *** *** ~ //
 // ~ ****************************** ~ //
-import { specs, serve, setup, cssOptions} from './swaggerDocs/swaggerDocs.js';
-app.use('/api-docs', serve, setup(specs, cssOptions));
-
+import { specs, serve, setup, cssOptions } from "./swaggerDocs/swaggerDocs.js";
+app.use("/api-docs", serve, setup(specs, cssOptions));
 
 // If you have your node.js behind a proxy and are using secure: true, you need to set 'trust proxy' in express
 app.set("trust proxy", 1); // trust first proxy
-
 
 // ~ *** *** SESSION CONFIG *** *** ~ //
 // ~ ****************************** ~ //
@@ -46,9 +44,16 @@ app.use(
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const PORT = process.env.PORT || 5000;
-const corsOptions = { credentials: true, origin: "https://e-co-roads.netlify.app" };
+// const corsOptions = { credentials: true, origin: "https://e-co-roads.netlify.app" };
 
-app.use(cors(corsOptions));
+app.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "https://e-co-roads.netlify.app");
+    res.setHeader("Access-Control-Allow-Methods", "*");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    next();
+});
+
+// app.use(cors(corsOptions));
 
 // ~ *** *** PARSER CONFIG *** *** ~ //
 // ~ ***************************** ~ //
@@ -58,7 +63,6 @@ app.use(cookieParser());
 
 app.use("/", express.static(join(__dirname, "public")));
 
-
 // ~ *** *** LAUNCHER CONFIG *** *** ~ //
 // ~ ******************************* ~ //
 app.use(router);
@@ -66,4 +70,3 @@ app.use(router);
 app.listen(PORT, () => {
     console.log(` \x1b[1;33m⚡⚡ http://localhost:${PORT} ⚡⚡ \x1b[0m`);
 });
-
