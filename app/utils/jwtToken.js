11a36e274495a294e7e6bcf672f53023caf8cb6e
GaetanSantucci <99250736+GaetanSantucci@users.noreply.github.com>
2022-07-26 09:08:44 +0200

    :bug: fix bug deploy

--- app/utils/jwtToken.js
+++ app/utils/jwtToken.js
@@ -2,10 +2,20 @@ import jwt from "jsonwebtoken";
 
 
 function generateAccessToken(user) {
+    // const jwtUser = {
+    //     id: user.id,
+    //     email: user.email,
+    //     username: user.username
+    // }
     return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1800s" });
 }
 
 function generateRefreshToken(user) {
+    // const jwtUser = {
+    //     id: user.id,
+    //     email: user.email,
+    //     username: user.username
+    // }
     return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: "36000s" });
 }
 
