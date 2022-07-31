cbd6851cef453702b3bbdf1c57d353e39e35977b
GaetanSantucci <99250736+GaetanSantucci@users.noreply.github.com>
2022-08-01 00:07:31 +0200

    :boom: update and reset database, test not ok user logout

--- db/revert/01.init.sql
+++
@@ -1,7 +0,0 @@
--- Revert ecoroads:01.init from pg
-
-BEGIN;
-
--- XXX Add DDLs here.
-
-COMMIT;
--- db/verify/02.add_table_brand.sql
+++
@@ -1,7 +0,0 @@
--- Verify ecoroads:02.add_table_brand on pg
-
-BEGIN;
-
-SELECT * FROM "car";
-
-ROLLBACK;
