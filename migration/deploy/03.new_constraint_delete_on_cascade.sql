896236688a375585846e5b0d1a282caa02a9b29e
GaetanSantucci <99250736+GaetanSantucci@users.noreply.github.com>
2022-08-04 11:28:33 +0200

    :boom: adjustement

--- migration/deploy/03.new_constraint_delete_on_cascade.sql
+++ migration/deploy/03.new_constraint_delete_on_cascade.sql
@@ -1,17 +1,11 @@
 -- Deploy ecoroads:03.new_constraint_delete_on_cascade to pg
 
 ALTER TABLE "user_like_category"
-drop CONSTRAINT "user_like_category_user_id_fkey";
-ALTER TABLE "user_like_category"
-drop CONSTRAINT "user_like_category_user_id_fkey";
-ALTER TABLE "user_like_category"
 ADD FOREIGN KEY ("user_id")
     REFERENCES "user"
         ("id")
     ON DELETE CASCADE ON UPDATE NO ACTION;
 
-ALTER TABLE "user_like_category"
-drop CONSTRAINT "user_like_category_category_id_fkey";
 
 ALTER TABLE "user_like_category"
 ADD FOREIGN KEY ("category_id")
